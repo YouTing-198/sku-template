c8626ffa6cffe4419b41c57a992fa66ed4b401df
YouTing <1982909183@qq.com>
2022-07-20 11:37:50 +0800

    feat(分类管理): 新增+推荐商品

--- src/api/category.js
+++ src/api/category.js
@@ -10,6 +10,26 @@ export const getCategoryListAPI = (data) => {
     data
   })
 }
+/**
+ * 添加分类
+ */
+export const addCategoryAPI = (data) => {
+  return request({
+    url: '/category',
+    method: 'POST',
+    data
+  })
+}
+/**
+ * 修改
+ */
+export const editCategoryAPI = (data) => {
+  return request({
+    url: `/category/${data.id}`,
+    method: 'POST',
+    data
+  })
+}
 
 /**
  * 推荐商品
@@ -19,5 +39,24 @@ export const appCategortItemAPI = (data) => {
     url: '/app_category_item/list',
     method: 'GET',
     data
+  })
+}
+/**
+ * 修改状态
+ */
+export const updateStatusAPI = (id, data) => {
+  return request({
+    url: `/category/${id}/update_status`,
+    method: 'POST',
+    data
+  })
+}
+/**
+ * 删除分类
+ */
+export const deleteCategoryAPI = (id) => {
+  return request({
+    url: `/category/${id}/delete`,
+    method: 'POST'
   })
 }
