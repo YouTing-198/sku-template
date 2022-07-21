517cc6bb7bd007fbef6a2a126d57ea703fc7d218
YouTing <1982909183@qq.com>
2022-07-21 18:52:36 +0800

    feat(优惠券管理): 渲染

--- src/api/coupon.js
+++ src/api/coupon.js
@@ -1,8 +1,10 @@
+// 优惠券模块
 import request from '@/utils/request'
+
 /**
- *  获取优惠券列表
+ * 优惠券列表
  */
-export const getCouponAPI = (current) => {
+export const couponListAPI = (current) => {
   return request({
     url: `/coupon/${current}`,
     method: 'GET'
