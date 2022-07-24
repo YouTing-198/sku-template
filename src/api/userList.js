2668cb038a6a4a5ef829114b59f576718efb3e07
YouTing <1982909183@qq.com>
2022-07-24 20:05:53 +0800

    feat(用户管理): 新增用户+修改用户

--- src/api/userList.js
+++ src/api/userList.js
@@ -13,3 +13,29 @@ export const userListAPI = (data) => {
     data: query
   })
 }
+/**
+ * 添加用户 修改用户
+ */
+export const addUserListAPI = (data) => {
+  const url = data.id ? `/user/${data.id}` : '/user'
+  return request({
+    url,
+    method: 'POST',
+    data
+  })
+}
+// 修改用户状态
+export const editUserStatusAPI = (id, status) => {
+  return request({
+    url: `/user/${id}/update_status`,
+    method: 'POST',
+    data: status
+  })
+}
+// 删除用户
+export const delUserListAPI = (id) => {
+  return request({
+    url: `/user/${id}/delete`,
+    method: 'POST'
+  })
+}
--- src/views/user_user-list_list/index.vue
+++ src/views/user_user-list_list/index.vue
@@ -13,7 +13,6 @@
         <el-form-item v-if="userLevelShow" label="会员等级">
           <el-select
             v-model="queryModel.user_level_id"
-            :props="{ label: 'name', value: 'id' }"
             placeholder="请选择会员等级"
           >
             <el-option
@@ -46,7 +45,9 @@
     </div>
     <!-- 新增+刷新 -->
     <div class="refresh">
-      <el-button size="small" type="primary">新增</el-button>
+      <el-button size="small" type="primary" @click="drawerVisable = true"
+        >新增</el-button
+      >
       <el-tooltip content="刷新数据" placement="top">
         <el-button icon="Refresh" text></el-button>
       </el-tooltip>
@@ -68,21 +69,113 @@
       <template #status="{ row }">
         <el-switch v-model="row.switchStatus"></el-switch>
       </template>
-      <template #action>
-        <el-button link type="primary">修改</el-button>
+      <template #action="{ row }">
+        <el-button link type="primary" @click="handleEdit(row)">修改</el-button>
         <el-button link type="primary">删除</el-button>
       </template>
     </ATable>
     <Paging :total="total" @currentChange="currentChange"></Paging>
+    <!-- 添加和编辑抽屉 -->
+    <el-drawer v-model="drawerVisable" size="40%">
+      <template #header>
+        <span>{{ drawerTitle }}</span>
+      </template>
+      <el-form
+        ref="userListFormRef"
+        :model="userListModel"
+        :rules="formRules"
+        label-width="80px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input
+            v-model.trim="userListModel.username"
+            :maxlength="25"
+            :min="4"
+          ></el-input>
+        </el-form-item>
+        <el-form-item :prop="userListModel.id ? '' : 'password'" label="密码">
+          <el-input
+            v-model.trim="userListModel.password"
+            show-password
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="昵称">
+          <el-input v-model.trim="userListModel.nickname"></el-input>
+        </el-form-item>
+        <el-form-item label="头像">
+          <el-upload list-type="picture-card">
+            <el-icon>
+              <Plus />
+            </el-icon>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="会员等级" porp="user_level_id">
+          <el-select
+            v-model="userListModel.user_level_id"
+            placeholder="请选择会员等级"
+          >
+            <el-option
+              v-for="item in userLevel"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="手机" prop="phone">
+          <el-input v-model.trim="userListModel.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model.trim="userListModel.email"></el-input>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-switch
+            v-model="userListModel.status"
+            :active-value="1"
+            :inactive-value="0"
+          ></el-switch>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="footer">
+          <el-button type="primary" @click="handleAddUser">提交</el-button>
+          <el-button type="default" @click="handleResetUserForm"
+            >取消</el-button
+          >
+        </div>
+      </template>
+    </el-drawer>
   </el-card>
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue'
-import { userListAPI } from '@/api/userList'
+import { computed, reactive, ref } from 'vue'
+import { userListAPI, addUserListAPI } from '@/api/userList'
+import { Notification } from '@/utils/Notification'
 import ATable from '@/components/Table/a-table'
 import column from './tableColumn'
 import Paging from '@/components/Paging'
+import formRules from './formRules'
+// 抽屉标题
+const drawerTitle = computed(() => {
+  return userListModel.id ? '修改' : '新增'
+})
+// 添加-修改表单ref
+const userListFormRef = ref(null)
+// 添加-修改数据模型
+const userListModel = reactive({
+  avatar: '',
+  email: '',
+  nickname: '',
+  password: '',
+  phone: '',
+  status: 1,
+  user_level_id: '',
+  username: ''
+})
+// 添加-编辑抽屉是否显示
+const drawerVisable = ref(false)
 // 查询模型
 const queryModel = reactive({
   current: 1,
@@ -134,6 +227,37 @@ const currentChange = (value) => {
   queryModel.current = value
   getUserList()
 }
+// 添加
+const handleAddUser = async () => {
+  try {
+    await userListFormRef.value.validate()
+    await addUserListAPI(userListModel)
+    await getUserList()
+    handleResetUserForm()
+    Notification(userListModel.id ? '修改成功' : '添加成功', '', 'success')
+  } catch (e) {
+    console.log(e)
+  }
+}
+// 重置用户表单
+const handleResetUserForm = async () => {
+  await userListFormRef.value.resetFields()
+  for (const key in userListModel) {
+    if (key === 'status') {
+      userListModel[key] = 1
+    } else {
+      userListModel[key] = ''
+    }
+  }
+  drawerVisable.value = false
+}
+// 修改用户
+const handleEdit = (row) => {
+  for (const rowKey in row) {
+    userListModel[rowKey] = row[rowKey]
+  }
+  drawerVisable.value = true
+}
 </script>
 <style lang="scss" scoped>
 .search {
@@ -172,5 +296,9 @@ const currentChange = (value) => {
       }
     }
   }
+}
+.footer {
+  display: flex;
+  justify-content: flex-start;
 }
 </style>
